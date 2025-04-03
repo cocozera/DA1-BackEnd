eb5db2d7d43bd8121667616cdaf7443e647e34da
baudelcerro1204 <bdelcerro@uade.edu.ar>
2025-04-03 11:06:01 -0300

    routeDetail

--- src/main/java/com/example/da1_backend/route/RouteController.java
+++ src/main/java/com/example/da1_backend/route/RouteController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/routes")
+@RequestMapping("/api/routes")
 public class RouteController {
 
     @Autowired
