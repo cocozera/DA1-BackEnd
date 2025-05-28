0e987c9439c770719586ea9413f69018f86c244b
emlomonaco <emlomonaco@uade.edu.ar>
2025-05-28 12:20:24 -0300

    .env

--- src/main/java/com/example/da1_backend/Da1BackendApplication.java
+++ src/main/java/com/example/da1_backend/Da1BackendApplication.java
@@ -1,5 +1,6 @@
 package com.example.da1_backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Da1BackendApplication {
 
     public static void main(String[] args) {
+        // Cargar variables de entorno desde .env
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+        System.setProperty("BREVO_APIKEY", dotenv.get("BREVO_APIKEY"));
+        System.setProperty("BREVO_SENDER_EMAIL", dotenv.get("BREVO_SENDER_EMAIL"));
+        System.setProperty("BREVO_SENDER_NAME", dotenv.get("BREVO_SENDER_NAME"));
+
+
         SpringApplication.run(Da1BackendApplication.class, args);
     }
-
 }
